548c8348c7fb83a1d937310c2e9042b1bb35bfd7
Simon-Za <64073972+Simon-Za@users.noreply.github.com>
2023-07-25 09:53:50 +0200

    Update MyNovel.js

--- MyNovel/Build/MyNovel.js
+++ MyNovel/Build/MyNovel.js
@@ -430,7 +430,7 @@ var MyNovel;
         gameMenu = MyNovel.ƒS.Menu.create(inGameMenuButtons, buttonFunctionalities, "gameMenuCSSClass");
         buttonFunctionalities("Close");
         let scenes = [
-            //{ scene: GameScene01, name: "OpeningFight"},
+            { scene: GameScene01, name: "OpeningFight"},
             { scene: MyNovel.GameScene02, name: "Exposition" },
             { id: "GameScene03Q1", scene: MyNovel.GameScene03Q1, name: "Quest1Start", next: "GameScene04Q1" },
             { id: "GameScene03Q2", scene: MyNovel.GameScene03Q2, name: "Quest2Start", next: "GameScene04Q2" },
